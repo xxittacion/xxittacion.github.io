bc2066651d0efa5b8adb1aebbc2a74f34deaab0a
Pavel Bogachev <99125126+xxittacion@users.noreply.github.com>
2022-09-23 17:21:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-let tg = window.Telegram.WebApp;
+let tg = window.Telegram.WebApp; // получаем объект webapp телеграмма 
 
-tg.expand();
+tg.expand(); // расширяем на все окно
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
+// tg.MainButton.text = "Text"; // изменяем текст кнопки 
+// tg.MainButton.setText("Text1"); // изменяем текст кнопки иначе
+// tg.MainButton.setParams({"color": "#143F6B"}); // так изменяются все параметры
+
+tg.MainButton.textColor = "#FFFFFF"; // изменяем цвет текста кнопки
+tg.MainButton.color = "#2cab37"; // изменяем цвет бэкграунда кнопки
 
 let item = "";
 
 let btn1, btn2, btn3, btn4, btn5, btn6;
 
 
+// при клике на кнопку назад, в шапке интерфейса
+tg.BackButton.onClick(() => {
+	window.location.href='index.html'; // переход назад
+	tg.BackButton.hide(); // кнопка назад скрыта
+});
+
 btn1 = document.getElementById("btn1").addEventListener("click", () => {
 	if (tg.MainButton.isVisible) { // нижняя кнопка видна
-		tg.MainButton.hide(); // нижнюю кнопка скрыта
+		tg.MainButton.hide(); // нижняя кнопка скрыта
 	}
 	else { // если нижняя кнопка скрыта
 		tg.MainButton.setText("Перейти в раздел: Кисти"); // текст изменён
@@ -21,20 +31,9 @@ btn1 = document.getElementById("btn1").addEventListener("click", () => {
 		// при клике на нижнюю кнопку
 		tg.MainButton.onClick(() => {
 			window.location.href='brushes.html'; // переход в выбранный раздел
-			tg.BackButton.show() // кнопка назад видна
+			tg.BackButton.show() // кнопка назад видна, в шапке интерфейса
 		})
 	}
-});
-
-tg.BackButton.onClick(() => {
-	window.location.href='index.html'; // переход назад
-	tg.BackButton.hide(); // кнопка назад скрыта
-});
-
-// при клике на кнопку назад, в шапке интерфейса телеграмма
-tg.BackButton.onEvent('backButtonClicked', () => {
-	window.location.href='index.html'; // переход назад
-	tg.BackButton.hide(); // кнопка назад скрыта
 });
 
 btn2 = document.getElementById("btn2").addEventListener("click", () => {
@@ -91,3 +90,19 @@ btn6 = document.getElementById("btn6").addEventListener("click", () => {
 		tg.MainButton.show();
 	}
 });
+
+
+// Добавляет имя и фамилию
+
+// Telegram.WebApp.onEvent("mainButtonClicked", ()=> {
+// 	tg.sendData(item);
+// });
+
+// let usercard = document.getElementById("usercard");
+
+// let p = document.createElement("p");
+
+// p.innerText = `${tg.initDataUnsafe.user.first_name}
+// ${tg.initDataUnsafe.user.last_name}`;
+
+// usercard.appendChild(p); 
